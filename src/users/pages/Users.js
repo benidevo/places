64d4b545a71d5b6ca34f529abd3d430e52e4e2b7
Benjamin Idewor <benjaminidewor@gmail.com>
2021-08-24 00:42:04 +0100

    Add users list

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-  return (
-    <div>
-      welcome to users page
-    </div>
-  )
-}
+  const USERS = [
+    {
+      id: 1,
+      name: 'Ben Idewor',
+      image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+      placesCount: 3
+    }
+  ]
+
+  return <UsersList items={USERS} />;
+};
 
 export default Users;
