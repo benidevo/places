7b9da43665bc732b20515f8e1dc128c482587ae9
Benjamin Idewor <benjaminidewor@gmail.com>
2021-08-23 23:55:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+
+import Users from './users/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Users} />
+        <Route path="/places/new" exact component={NewPlace} />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  )
+}
+
+export default App;
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewPlace = () => {
+  return (
+    <div>
+      This is a new place
+    </div>
+  )
+}
+
+export default NewPlace;
